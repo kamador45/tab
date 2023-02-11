388e30ae45868e04d0483d81b10b0cf5f5bec9ce
Kamador <kamador@ocfnyc.com>
2023-02-11 15:58:19 -0600

    Initial Commit

---
+++ TabIndicadors/TabIndicadorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabIndicadorsApp.swift
+//  TabIndicadors
+//
+//  Created by Kevin Amador Rios on 2/11/23.
+//
+
+import SwiftUI
+
+@main
+struct TabIndicadorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
